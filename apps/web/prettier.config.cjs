dd3fd1fa79369e2dace318f66eb3c0e802217214
vatsal <0505m2003@gmail.com>
2023-02-23 10:13:09 +0530

    Adding configs and other packages

--- apps/web/prettier.config.cjs
+++ apps/web/prettier.config.cjs
@@ -1,4 +1,26 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  endOfLine: "lf",
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
+  semi: true,
+  arrowParens: "avoid",
+  singleQuote: true,
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    "@ianvs/prettier-plugin-sort-imports",
+  ],
+  importOrder: [
+    "^react",
+    "<THIRD_PARTY_MODULES>",
+    "<TYPES>",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderSortSpecifiers: true,
+  importOrderCombineTypeAndValueImports: true,
+  importOrderBuiltinModulesToTop: true,
 };
